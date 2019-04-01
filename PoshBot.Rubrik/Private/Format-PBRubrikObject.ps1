34341cafa4454954d9010915241b86293f5ec9cb
Jaap Brasser <noreply@jaapbrasser.com>
2019-04-01 12:39:42 -0800

    Finalized formatting function

--- PoshBot.Rubrik/Private/Format-PBRubrikObject.ps1
+++ PoshBot.Rubrik/Private/Format-PBRubrikObject.ps1
@@ -1,4 +1,16 @@
 function Format-PBRubrikObject {
+<#
+.SYNOPSIS
+Unified function to format output to be used by the public functions
+
+.DESCRIPTION
+Format the output as strings and with a set width, will display different objects based on the function that called it
+
+.NOTES
+Written by Jaap Brasser for community usage
+Twitter: @jaap_brasser
+GitHub: jaapbrasser
+#>
     param (
         # The objects returned by the API, will be reformatted
         [Parameter(
@@ -12,10 +24,18 @@ function Format-PBRubrikObject {
     )
 
     if (($object.count -eq 0) -or (-not $object)) {
-        $msg = 'No virtual machines found'
-    } else {
-        $msg = ($objects | Format-List | Out-String -Width 120)
-    }
+        $msg = 'No {0} found'
+        switch ($functionname) {
+            'Get-PBRubrikDatabase' {$msg -f 'databases'}
+            'Get-PBRubrikReport' {$msg -f 'reports'}
+            'Get-PBRubrikSLA' {$msg -f 'SLAs'}
+            'Get-PBRubrikSnapshot' {$msg -f 'Snapshots'}
+            'Get-PBRubrikVersion' {$msg -f 'version information'}
+            'Get-PBRubrikVM' {$msg -f 'virtual machines'}
+            default {$msg -f 'objects'}
+        }
 
-    return $msg
+    } else {
+        $objects | Format-List | Out-String -Width 120
+    }
 }
