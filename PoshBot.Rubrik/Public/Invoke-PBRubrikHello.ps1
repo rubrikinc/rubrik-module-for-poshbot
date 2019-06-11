bcc06ebe80c3d29ce7061bc7e2e3e17108635301
Jaap Brasser <noreply@jaapbrasser.com>
2019-06-11 22:36:43 +0200

    Added initial support for Chinese language

--- PoshBot.Rubrik/Public/Invoke-PBRubrikHello.ps1
+++ PoshBot.Rubrik/Public/Invoke-PBRubrikHello.ps1
@@ -9,20 +9,21 @@ function Invoke-PBRubrikHello {
         [string] $Language
     )
 
-    if ($Language) {
+    if ($Language -eq 'Chinese') {
         $ResponseSplat = @{
-            Text = 'Hello, I am Roxie! Today I am on running version 1.0.0.0 of the Rubrik Module for PoshBot!'
+            Text = '你好，我是罗茜！今天我将正式运行为PoshBot搭建的Rubrik模块 1.0.0.0 版本！'
         }
         New-PoshBotTextResponse @ResponseSplat
 
         Start-Sleep -Seconds 1
 
         $ResponseSplat = @{
-            Text = 'I have 10 Rubrik commands available, you can view the available commands by asking me for help
+            Text = '我拥有10个可供选择的Rubrik命令，你可以通过向我提问来了解它们
 ```
 roxie gp poshbot.rubrik
 ```
 '
+        }
     } else {
         $ResponseSplat = @{
             Text = 'Hello, I am Roxie! Today I am on running version 1.0.0.0 of the Rubrik Module for PoshBot!'
