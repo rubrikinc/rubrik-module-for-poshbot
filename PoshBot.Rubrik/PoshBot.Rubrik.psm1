782463f1742493e1e7ceb7bc3fc686e66ef98636
Jaap Brasser <noreply@jaapbrasser.com>
2019-09-05 13:46:53 +0200

    Fixed function import for Linux and MacOS systems

--- PoshBot.Rubrik/PoshBot.Rubrik.psm1
+++ PoshBot.Rubrik/PoshBot.Rubrik.psm1
@@ -1,5 +1,5 @@
 'Private','Public' | ForEach-Object {
-    [io.directory]::getfiles("$PSScriptRoot\$_") | ForEach-Object {
+    [io.directory]::getfiles("$PSScriptRoot/$_") | ForEach-Object {
         $functioncontent = [io.file]::readalltext($_)
         $ExecutionContext.InvokeCommand.InvokeScript($functioncontent, $false, [Management.Automation.Runspaces.PipelineResultTypes]::None, $null, $null)
     }
